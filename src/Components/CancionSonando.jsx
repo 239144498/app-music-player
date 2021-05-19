203c05fb022df0c0535a9480f902b1178d8e78b9
julian0394 <julian0394@hotmail.com>
2021-05-19 17:31:18 -0300

    Terminadas las media queries y giro de portada

--- src/Components/CancionSonando.jsx
+++ src/Components/CancionSonando.jsx
@@ -1,12 +1,13 @@
 
-const CancionSonando = ( {cancion} ) => {
-    return ( 
-        <div className='contenedor-cancion'>
-            <img className="no-selecionable" src={cancion.portada} alt="Portada del disco" />
-            <h2>{cancion.nombre}</h2>
-            <h3>{cancion.artista}</h3>
-        </div>
-     );
+const CancionSonando = ( {cancion, estaSonando} ) => {
+  return ( 
+    <div className='contenedor-cancion'>
+      {/* <img className='no-selecionable' src={cancion.portada} alt="Portada del disco" />       */}
+      <img className={`no-selecionable ${estaSonando && 'imagen-pulso'}`} src={cancion.portada} alt="Portada del disco" />      
+      <h2>{cancion.nombre}</h2>
+      <h3>{cancion.artista}</h3>
+    </div>
+  );
 }
  
 export default CancionSonando;
