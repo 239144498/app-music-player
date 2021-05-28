220f747d04226a77f6f3eb029b6badc399955743
julian0394 <julian0394@hotmail.com>
2021-05-28 17:00:24 -0300

    Agregado favicon

--- src/util.js
+++
@@ -1,7 +0,0 @@
-export const chequearContinuar = (estaSonando, audioRef) => {
-  if(estaSonando) {
-    const promesaReproducir = audioRef.current.play();
-    if(promesaReproducir !== undefined)
-      promesaReproducir.then( audio => audioRef.current.play() );
-  };
-};
