02723569f7789dd8f7c4d97d017b501357c53c18
Naihe <64341459+239144498@users.noreply.github.com>
2024-10-21 17:30:01 +0800

    Update cancionesChillHop.js

--- src/cancionesChillHop.js
+++ src/cancionesChillHop.js
@@ -4,13 +4,21 @@ function cancionesChillHop() {
   return [
     {
       id: uuidv4(),
-      nombre: "随机",
+      nombre: "随机1",
       portada: "https://img.icons8.com/fluency/48/music.png", 
-      artista: "随机",
-      audio: "https://music.naiher.me/",
+      artista: "随机1",
+      audio: "https://music.naiher.me/random1",
       colores: ["#FDD0BD", "#564E89"],
       reproduciendo: true
-    }   
+    },{
+      id: uuidv4(),
+      nombre: "随机2",
+      portada: "https://img.icons8.com/fluency/48/music.png", 
+      artista: "随机2",
+      audio: "https://music.naiher.me/random2",
+      colores: ["#FDD0BD", "#564E89"],
+      reproduciendo: true
+    } 
   ];
 }
 
