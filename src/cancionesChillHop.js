6584cb3f5b3bb95a5a912ebfd4d84097ad51bd21
Naihe <64341459+239144498@users.noreply.github.com>
2024-10-21 17:25:25 +0800

    Update cancionesChillHop.js

--- src/cancionesChillHop.js
+++ src/cancionesChillHop.js
@@ -4,94 +4,13 @@ function cancionesChillHop() {
   return [
     {
       id: uuidv4(),
-      nombre: "Daylight",
-      portada: "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg", 
-      artista: "Aiguille",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=6517",
+      nombre: "随机",
+      portada: "https://img.icons8.com/fluency/48/music.png", 
+      artista: "随机",
+      audio: "https://music.naiher.me/",
       colores: ["#FDD0BD", "#564E89"],
       reproduciendo: true
-    },
-    {
-      id: uuidv4(),
-      nombre: "Flashback",
-      portada: "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-1024x1024.jpg", 
-      artista: "Blue Wednesday, Shopan",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=11224",
-      colores: ["#AAA7DC", "#B78DBB"],
-      reproduciendo: false
-    },
-    {
-      id: uuidv4(),
-      nombre: "Reflect",
-      portada: "https://chillhop.com/wp-content/uploads/2021/03/74d62bc9370a68e440c1b98eaf650344f0a7faea-1024x1024.jpg", 
-      artista: "SwuM, Ben Belial",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=15234",
-      colores: ["#CDA172", "#2E3732"],
-      reproduciendo: false
-    },
-    {
-      id: uuidv4(),
-      nombre: "Woodnote",
-      portada: "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg", 
-      artista: "Aarigod, imagiro",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=13011",
-      colores: ["#ACC96F", "#2C281C"],
-      reproduciendo: false
-    },
-    {
-      id: uuidv4(),
-      nombre: "Lilo",
-      portada: "https://chillhop.com/wp-content/uploads/2020/11/8a0c857ddad531279d0757f5362380a6837b1b69-1024x1024.jpg", 
-      artista: "Middle School, The Field Tapes",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=11237",
-      colores: ["#DBE8DF", "#525560"],
-      reproduciendo: false
-    },
-    {
-      id: uuidv4(),
-      nombre: "Departure",
-      portada: "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg", 
-      artista: "Makzo",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=11769",
-      colores: ["#F2AC66", "#4F64A3"],
-      reproduciendo: false
-    },
-    {
-      id: uuidv4(),
-      nombre: "Habitat",
-      portada: "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-1024x1024.jpg", 
-      artista: "Philanthrope, Kendall Miles",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10264",
-      colores: ["#DF8763", "#AB4237"],
-      reproduciendo: false
-    },
-    {
-      id: uuidv4(),
-      nombre: "Backyard Puddles",
-      portada: "https://chillhop.com/wp-content/uploads/2020/09/c209a7df7b9bc133dfff73ce86ebc3c57c2b73dd-1024x1024.jpg", 
-      artista: "Sleepy Fish",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=10032",
-      colores: ["#F1E2C5", "#3886A6"],
-      reproduciendo: false
-    },
-    {
-      id: uuidv4(),
-      nombre: "Reflection",
-      portada: "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg", 
-      artista: "Swørn",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=6552",
-      colores: ["#D56F84", "#7E427E"],
-      reproduciendo: false
-    },  
-    {
-      id: uuidv4(),
-      nombre: "Vintage",
-      portada: "https://chillhop.com/wp-content/uploads/2021/04/cb0cc6270d7f2e1bb13e44e8832bd5c9b2a61080-1024x1024.jpg", 
-      artista: "Evil Needle",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=17088",
-      colores: ["#EB8556", "#EBC77D"],
-      reproduciendo: false
-    },   
+    }   
   ];
 }
 
